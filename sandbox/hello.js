c0f86062fb942e797b0326416818c88dc6dcb408
tomo <tomopepe.new.3@gmail.com>
2026-05-31 19:50:16 +0900

    3002:Hello World2!!'

--- sandbox/hello.js
+++ sandbox/hello.js
@@ -2,10 +2,9 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-    res.end('Hello! Server is working!');
+    res.end('Hello World2!!');
 });
 
 server.listen(3002, '0.0.0.0', () => {
     console.log('Server is running on port 3002');
 });
-
