ba4d3dd2e34773bb7521b53c18c5b29f082614c4
tomo <tomopepe.new.3@gmail.com>
2026-05-31 15:59:05 +0900

    Create a simple HTTP server in hello.js

---
+++ sandbox/hello.js
@@ -0,0 +1,9 @@
+constserver = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+    res.end('Hello! Server is working!');
+});
+
+server.listen(3001, '0.0.0.0', () => {
+    console.log('Server is running on port 3001');
+});
+});
